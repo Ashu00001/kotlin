de5db7ab64bc861bbd055ffbd5b6b712c88d3dc7
Ashutosh Gupta <43481150+Ashu00001@users.noreply.github.com>
2021-06-12 09:59:18 +0530

    Update nullsafety.kt

--- nullsafety.kt
+++ nullsafety.kt
@@ -1,1 +1,10 @@
-
+fun main() {
+    var name: String? =null
+   printname("ashu","gupta")
+}
+fun printname(firstname:String? ,lastname:String?)
+{
+    
+        println("Name : ${firstname!!.length} ${lastname!!.length}")
+    
+}
